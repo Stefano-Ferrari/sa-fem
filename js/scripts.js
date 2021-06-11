1925c6167b4efd26568fd164cc1ddcf979b0736e
Stefano-Ferrari <36973253+Stefano-Ferrari@users.noreply.github.com>
2021-06-12 00:39:38 +0200

    modifiche logotipo e pagina about

--- js/scripts.js
+++ js/scripts.js
@@ -65,7 +65,7 @@ L.tileLayer('https://api.mapbox.com/styles/v1/steferro97/ckp6n7s3p278r17phg7hcg7
 var marker1 = L.marker([45.50554, 9.16517], {icon: customPin}).addTo(map1);
 
 
-var map2 = L.map('map1').setView([45.50554, 9.16517], 13);
+var map2 = L.map('map2').setView([45.50554, 9.16517], 13);
 mapLink = '<a href="http://openstreetmap.org">OpenStreetMap</a>';
 L.tileLayer('https://api.mapbox.com/styles/v1/steferro97/ckp6n7s3p278r17phg7hcg7h7/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic3RlZmVycm85NyIsImEiOiJjazU0OGJmb3EwNDF3M21wNWExMGd6YWhkIn0.WC7fwaVH6_71tm2RVUllaQ', {
   attribution: 'Map data &copy; ' + mapLink,
@@ -75,7 +75,7 @@ L.tileLayer('https://api.mapbox.com/styles/v1/steferro97/ckp6n7s3p278r17phg7hcg7
 var marker2 = L.marker([45.50554, 9.16517], {icon: customPin}).addTo(map2);
 
 
-var map3 = L.map('map1').setView([45.50554, 9.16517], 13);
+var map3 = L.map('map3').setView([45.50554, 9.16517], 13);
 mapLink = '<a href="http://openstreetmap.org">OpenStreetMap</a>';
 L.tileLayer('https://api.mapbox.com/styles/v1/steferro97/ckp6n7s3p278r17phg7hcg7h7/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic3RlZmVycm85NyIsImEiOiJjazU0OGJmb3EwNDF3M21wNWExMGd6YWhkIn0.WC7fwaVH6_71tm2RVUllaQ', {
   attribution: 'Map data &copy; ' + mapLink,
@@ -85,7 +85,7 @@ L.tileLayer('https://api.mapbox.com/styles/v1/steferro97/ckp6n7s3p278r17phg7hcg7
 var marker3 = L.marker([45.50554, 9.16517], {icon: customPin}).addTo(map3);
 
 
-var map4 = L.map('map1').setView([45.50554, 9.16517], 13);
+var map4 = L.map('map4').setView([45.50554, 9.16517], 13);
 mapLink = '<a href="http://openstreetmap.org">OpenStreetMap</a>';
 L.tileLayer('https://api.mapbox.com/styles/v1/steferro97/ckp6n7s3p278r17phg7hcg7h7/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic3RlZmVycm85NyIsImEiOiJjazU0OGJmb3EwNDF3M21wNWExMGd6YWhkIn0.WC7fwaVH6_71tm2RVUllaQ', {
   attribution: 'Map data &copy; ' + mapLink,
@@ -95,7 +95,7 @@ L.tileLayer('https://api.mapbox.com/styles/v1/steferro97/ckp6n7s3p278r17phg7hcg7
 var marker4 = L.marker([45.50554, 9.16517], {icon: customPin}).addTo(map4);
 
 
-var map5 = L.map('map1').setView([45.50554, 9.16517], 13);
+var map5 = L.map('map5').setView([45.50554, 9.16517], 13);
 mapLink = '<a href="http://openstreetmap.org">OpenStreetMap</a>';
 L.tileLayer('https://api.mapbox.com/styles/v1/steferro97/ckp6n7s3p278r17phg7hcg7h7/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic3RlZmVycm85NyIsImEiOiJjazU0OGJmb3EwNDF3M21wNWExMGd6YWhkIn0.WC7fwaVH6_71tm2RVUllaQ', {
   attribution: 'Map data &copy; ' + mapLink,
@@ -105,7 +105,7 @@ L.tileLayer('https://api.mapbox.com/styles/v1/steferro97/ckp6n7s3p278r17phg7hcg7
 var marker5 = L.marker([45.50554, 9.16517], {icon: customPin}).addTo(map5);
 
 
-var map6 = L.map('map1').setView([45.50554, 9.16517], 13);
+var map6 = L.map('map6').setView([45.50554, 9.16517], 13);
 mapLink = '<a href="http://openstreetmap.org">OpenStreetMap</a>';
 L.tileLayer('https://api.mapbox.com/styles/v1/steferro97/ckp6n7s3p278r17phg7hcg7h7/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic3RlZmVycm85NyIsImEiOiJjazU0OGJmb3EwNDF3M21wNWExMGd6YWhkIn0.WC7fwaVH6_71tm2RVUllaQ', {
   attribution: 'Map data &copy; ' + mapLink,
@@ -115,10 +115,28 @@ L.tileLayer('https://api.mapbox.com/styles/v1/steferro97/ckp6n7s3p278r17phg7hcg7
 var marker6 = L.marker([45.50554, 9.16517], {icon: customPin}).addTo(map6);
 
 function update_map(n) {
-
+  console.log('fuori'+n);
   $('#info-card' + n).on('shown.bs.modal', function(event) {
 
-    map1.invalidateSize();
+    if(n == 1){
+      map1.invalidateSize();
+      console.log('dentro'+n);
+    }else if(n == 2){
+      map2.invalidateSize();
+      console.log('dentro'+n);
+    }else if(n == 3){
+      map3.invalidateSize();
+      console.log('dentro'+n);
+    }else if(n == 4){
+      map4.invalidateSize();
+      console.log('dentro'+n);
+    }else if(n == 5){
+      map5.invalidateSize();
+      console.log('dentro'+n);
+    }else if(n == 6){
+      map6.invalidateSize();
+      console.log('dentro'+n);
+    }
 
   });
 
